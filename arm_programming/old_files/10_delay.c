cc930717ffa2ffcaf2f4228fde32b2b09e15df8d
marwan-nabil <marwan.nabil.mohammed@gmail.com>
2020-04-21 22:44:47 +0200

    separated arm_programming into a separate project

--- arm_programming/old_files/10_delay.c
+++
@@ -1,7 +0,0 @@
-#include "delay.h"
-
-void delay(volatile int iterations){
-    while(iterations > 0){
-        --iterations;
-    }
-}
--- arm_programming/old_files/9_delay.c
+++
@@ -1,8 +0,0 @@
-#include "delay.h"
-
-void delay(int iterations){
-    volatile int counter = 0;
-    while(counter < iterations){
-        ++counter;
-    }
-}
